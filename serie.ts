029d4668009a0b0394dda19203d1a690ce0b84fb
Juan-David <jd.torresa1@uniandes.edu.co>
2024-10-15 21:05:58 -0500

    Refactor Serie class constructor to include channel and seasons properties

--- serie.ts
+++ serie.ts
@@ -1,17 +1,17 @@
 export class Serie{
     id: number;
     name: string;
-    producer: string;
-    rating: number;
+    channel: string;
+    seasons: number;
     description: string;
     url: string;
     image: string;
 
-    constructor(id: number, name: string, producer: string, rating: number, description: string, url: string, image: string){
+    constructor(id: number, name: string, channel: string, seasons: number, description: string, url: string, image: string){
         this.id = id;
         this.name = name;
-        this.producer = producer;
-        this.rating = rating;
+        this.channel = channel;
+        this.seasons = seasons;
         this.description = description;
         this.url = url;
         this.image = image;
