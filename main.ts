94661ba972cc27c7bf148266396e0c62dcbaf820
Juan-David <jd.torresa1@uniandes.edu.co>
2024-10-16 18:33:43 -0500

    Refactor Serie class constructor to include channel and seasons properties

--- main.ts
+++ main.ts
@@ -4,12 +4,12 @@ import { dataSeries } from './data.js';
 
 const seriesTbody: HTMLElement = document.getElementById('series')!;
 const averageSeasonsElm: HTMLElement = document.getElementById('average-seasons')!;
-
+const cardSpace: HTMLElement = document.getElementById('card-space')!;
 renderSeriesTable(dataSeries);
 
-averageSeasonsElm.innerHTML = `${getAverageSeasons(dataSeries)}`;  
+averageSeasonsElm.innerHTML = `${getAverageSeasons(dataSeries)}`;
 
-function renderSeriesTable(series: Serie[]):void {
+function renderSeriesTable(series: Serie[]): void {
     seriesTbody.innerHTML = '';
     series.forEach((serie) => {
         let trElement = document.createElement('tr');
@@ -21,6 +21,7 @@ function renderSeriesTable(series: Serie[]):void {
             <td>${serie.seasons}</td>
             </td>
         `;
+        trElement.addEventListener('click', () => showInfoSerie(serie));
         seriesTbody.appendChild(trElement);
     });
 }
@@ -29,4 +30,18 @@ function getAverageSeasons(series: Serie[]): number {
     let totalSeasons: number = 0;
     series.forEach((serie) => totalSeasons = totalSeasons + serie.seasons);
     return totalSeasons / series.length;
-  }
+}
+
+function showInfoSerie(serie: Serie) {
+    console.log(serie.image);
+    cardSpace.innerHTML = `
+    <div class="card" style="width: 18rem;">
+        <img class="card-img-top" src="${serie.image}" alt="Imagen de portada de la serie ${serie.name}">
+        <div class="card-body">
+            <h5 class="card-title">${serie.name}</h5>
+            <p class="card-text">${serie.description}</p>
+            <a href="${serie.url}" class="btn btn-primary">Ir al sitio de la serie</a>
+        </div>
+    </div>
+    `;
+}
--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,7 @@
 import { dataSeries } from './data.js';
 var seriesTbody = document.getElementById('series');
 var averageSeasonsElm = document.getElementById('average-seasons');
+var cardSpace = document.getElementById('card-space');
 renderSeriesTable(dataSeries);
 averageSeasonsElm.innerHTML = "".concat(getAverageSeasons(dataSeries));
 function renderSeriesTable(series) {
@@ -9,6 +10,7 @@ function renderSeriesTable(series) {
         var trElement = document.createElement('tr');
         trElement.className = 'bg-light';
         trElement.innerHTML = "\n            <td class=\"font-weight-bold\">".concat(serie.id, "</td>\n            <td class=\"text-primary\">").concat(serie.name, "</td>\n            <td>").concat(serie.channel, "</td>\n            <td>").concat(serie.seasons, "</td>\n            </td>\n        ");
+        trElement.addEventListener('click', function () { return showInfoSerie(serie); });
         seriesTbody.appendChild(trElement);
     });
 }
@@ -16,4 +18,8 @@ function getAverageSeasons(series) {
     var totalSeasons = 0;
     series.forEach(function (serie) { return totalSeasons = totalSeasons + serie.seasons; });
     return totalSeasons / series.length;
+}
+function showInfoSerie(serie) {
+    console.log(serie.image);
+    cardSpace.innerHTML = "\n    <div class=\"card\" style=\"width: 18rem;\">\n        <img class=\"card-img-top\" src=\"".concat(serie.image, "\" alt=\"Imagen de portada de la serie ").concat(serie.name, "\">\n        <div class=\"card-body\">\n            <h5 class=\"card-title\">").concat(serie.name, "</h5>\n            <p class=\"card-text\">").concat(serie.description, "</p>\n            <a href=\"").concat(serie.url, "\" class=\"btn btn-primary\">Ir al sitio de la serie</a>\n        </div>\n    </div>\n    ");
 }
